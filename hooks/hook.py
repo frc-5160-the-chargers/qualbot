a010af10ad31b5ee95386ea0633f1929b09508d9
morzack <john_kesler@caryacademy.org>
2019-09-01 00:54:34 -0400

    added way for hooks to store data locally

--- hooks/hook.py
+++ hooks/hook.py
@@ -1,17 +1,34 @@
 import discord
 import json
 import traceback
+import time
 
 from fancy_logger import Logger
 
 class Hook:
     def __init__(self, hook_name, config_file="config.json"):
+        self.config_file = config_file
+        self.hook_name = hook_name
         with open(config_file, 'r') as f:
             data = json.load(f)
             self.config_data = data["webhooks"][hook_name]
+            self.delay = self.config_data["sending-delay"]
         self.logger = Logger(hook_name, config_file=config_file)
         self.logger.info("Initialized")
     
+    def update_hook_data(self, key, value):
+        '''write data specific to this hook for internal usage to the config file'''
+        self.config_data["hook-data"][key] = value
+        with open(self.config_file, 'r') as f:
+            data = json.load(f)
+        with open(self.config_file, 'w') as f:
+            data["webhooks"][self.hook_name] = self.config_data
+            json.dump(data, f)
+
+    def get_hook_data(self, key):
+        '''get a value from the cached hook data'''
+        return self.config_data["hook-data"][key]
+
     def generate_embed(self):
         '''this function should be overridden by other objects. generate the embed for the webhook'''
         pass
@@ -19,6 +36,10 @@ class Hook:
     def send(self):
         '''this shouldn't be overridden. sends the hoook created by generate_embed'''
         self.logger.info("Sending embed...")
+        last_sent_time = int(time.time()-self.get_hook_data("last-sent"))
+        if last_sent_time < self.delay:
+            self.logger.warning(f"Not sent:\n\tLast sent {last_sent_time}s ago")
+            return
         embed = None
         try:
             embed = self.generate_embed()
@@ -35,3 +56,8 @@ class Hook:
                 self.logger.error(f"Error while sending embed:\n\t{e}")
         else:
             self.logger.warning("Generated embed was empty")
+        try:
+            self.update_hook_data("last-sent", int(time.time()))
+        except Exception as e:
+            traceback.print_exc()
+            self.logger.error(f"Error recording last time sent:\n\t{e}")
