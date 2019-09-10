9b9c026198f3e547c0d976d2d79f790b68c7bf0b
morzack <john_kesler@caryacademy.org>
2019-09-09 22:14:50 -0400

    added support for slack

--- hooks/hook.py
+++ hooks/hook.py
@@ -5,6 +5,7 @@ import time
 import base64
 
 from fancy_logger import Logger
+from slack import send_message_to_slack
 
 class Hook:
     def __init__(self, hook_name, config_file="config.json"):
@@ -34,6 +35,14 @@ class Hook:
         '''this function should be overridden by other objects. generate the embed for the webhook'''
         pass
 
+    def send_to_service(self, embed):
+        '''this should be overridden based on the type of service'''
+        pass
+
+    def get_embed_str(self, embed):
+        '''get a string representation of an embed'''
+        return ""
+
     def send(self):
         '''this shouldn't be overridden. sends the hoook created by generate_embed'''
         self.logger.info("Sending embed...")
@@ -49,14 +58,14 @@ class Hook:
             self.logger.error("Error generating embed:\n\t{}".format(e))
         if embed != None:
             try:
-                last_embed_str = str(base64.b64encode(json.dumps(embed.to_dict(), sort_keys=True).encode('utf-8')), 'utf-8')
+                last_embed_str = self.get_embed_str(embed)
                 if last_embed_str == self.get_hook_data("last-sent-b64"):
                     self.logger.warning("This hook has already been sent")
                     return
                 self.update_hook_data("last-sent-b64", last_embed_str)
+
+                self.send_to_service(embed)
                 
-                discord_hook = discord.Webhook.partial(self.config_data["discord-id"], self.config_data["discord-token"], adapter=discord.RequestsWebhookAdapter())
-                discord_hook.send(embed=embed)
                 self.logger.info("Embed sent")
             except Exception as e:
                 traceback.print_exc()
@@ -68,3 +77,24 @@ class Hook:
         except Exception as e:
             traceback.print_exc()
             self.logger.error("Error recording last time sent:\n\t{}".format(e))
+
+class DiscordHook(Hook):
+    def __init__(self, hook_name, config_file="config.json"):
+        super().__init__(hook_name, config_file)
+
+    def get_embed_str(self, embed):
+        return str(base64.b64encode(json.dumps(embed.to_dict(), sort_keys=True).encode('utf-8')), 'utf-8')
+
+    def send_to_service(self, embed):
+        discord_hook = discord.Webhook.partial(self.config_data["discord-id"], self.config_data["discord-token"], adapter=discord.RequestsWebhookAdapter())
+        discord_hook.send(embed=embed)
+
+class SlackHook(Hook):
+    def __init__(self, hook_name, config_file="config.json"):
+        super().__init__(hook_name, config_file)
+
+    def get_embed_str(self, embed):
+        return str(base64.b64encode(json.dumps(embed, sort_keys=True).encode('utf-8')), 'utf-8')
+
+    def send_to_service(self, embed):
+        send_message_to_slack(embed, self.config_data["slack-url"], self.logger)
--- run_hooks.py
+++ run_hooks.py
@@ -1,15 +1,23 @@
 import sys
 
-from hooks.match_announce import MatchAnnouncementHook
+from hooks.match_announce import MatchAnnouncementHookDiscord, MatchAnnouncementHookSlack
 
 config_file = "config.json"
 
-def run_queuing_lady():
-    queuing_lady = MatchAnnouncementHook(config_file=config_file)
+def run_queuing_lady_discord():
+    queuing_lady = MatchAnnouncementHookDiscord(config_file=config_file)
     queuing_lady.send()
 
+def run_queuing_lady_slack():
+    queuing_lady = MatchAnnouncementHookSlack(config_file=config_file)
+    queuing_lady.send()
+
+def run_queuing_lady():
+    run_queuing_lady_discord()
+    run_queuing_lady_slack()
+
 def main():
-    available_hooks = ["", "queuing-lady"]
+    available_hooks = ["", "queuing-lady-discord", "queuing-lady-slack", "queuing-lady"]
     if len(sys.argv) != 2 or sys.argv[1] not in available_hooks:
         print("Make sure the hook entered is valid")
         print("Configured hooks:", end="")
@@ -17,6 +25,12 @@ def main():
         return
     hook_running = sys.argv[1]
     # run the hook
+    if hook_running in ["queuing-lady-discord"]:
+        run_queuing_lady_discord()
+        return
+    if hook_running in ["queuing-lady-slack"]:
+        run_queuing_lady_slack()
+        return
     if hook_running in ["queuing-lady"]:
         run_queuing_lady()
         return
